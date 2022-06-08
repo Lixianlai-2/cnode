dc106edf1e95cddd605658d1cd80c852f76036e7
MarkLee <1643565650@qq.com>
2022-06-08 22:42:45 +0800

    初步设置好header组件

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app works">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+@import "./assets/reset.css";
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="header">
+    <img src="../assets/cnodejs_light.svg" alt="cnode标志" />
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手入门</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于</a></li>
+      <li><a href="#">注册</a></li>
+      <li><a href="#">登录</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  /* border: 3px solid red; */
+  background: #5a5555;
+  justify-content: space-between;
+}
+
+img {
+  width: 100px;
+  padding: 10px;
+}
+
+ul {
+  padding-left: 100px;
+}
+
+li {
+  display: inline-block;
+  padding-right: 20px;
+}
+
+a {
+  color: #cccccc;
+}
+</style>
